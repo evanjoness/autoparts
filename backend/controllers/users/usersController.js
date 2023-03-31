1d48217ee46ce282cfeb4eccd0dfabf571839854
Ivan Shpytchuk <ivan7shpytchuk@gmail.com>
2023-03-31 20:53:58 +0300

    create admin

--- backend/controllers/users/usersController.js
+++ backend/controllers/users/usersController.js
@@ -1,11 +1,33 @@
 const { validationResult } = require("express-validator");
-
-
-module.exports.register = (req, res) => {
+const UserModel = require("../../models/User");
+const { hashedPassword } = require("../../services/authServices")
+//@route POST /api/register
+//@access Public 
+//@desc Create user and return a token 
+module.exports.register = async (req, res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        console.log(errors.array());
+    if (errors.isEmpty()) {
+        const { name, email, password } = req.body;
+        try {
+            const emailExist = await UserModel.findOne({ email });
+            if (!emailExist) {
+                const hashed = await hashedPassword(password);
+                const user = await UserModel.create({
+                    name,
+                    email,
+                    password: hashed
+                });
+                return res.status(201).json({ msg: "Your account has been created" });
+            } else {
+                return res.status(401).json({ errors: [{ msg: `${email} is already taken`, param: "email" }] })
+            }
+
+        } catch (error) {
+            console.log(error.message);
+            return res.status(500).json("server internal error");
+        }
     } else {
-        console.log(req.body);
+        //validations failed 
+        return res.status(400).json({ errors: errors.array() })
     }
 }
