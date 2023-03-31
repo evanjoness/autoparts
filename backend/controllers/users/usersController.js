e62a256c6f950ee1053dda7b71614d4a1ed5d552
Ivan Shpytchuk <ivan7shpytchuk@gmail.com>
2023-03-31 16:44:00 +0300

    Input Validations

---
+++ backend/controllers/users/usersController.js
@@ -0,0 +1,11 @@
+const { validationResult } = require("express-validator");
+
+
+module.exports.register = (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.array());
+    } else {
+        console.log(req.body);
+    }
+}
---
+++ backend/validations/userValidations.js
@@ -0,0 +1,7 @@
+const { body } = require("express-validator")
+
+module.exports.registerValidations = [
+    body("name").not().isEmpty().trim().escape().withMessage("Name is required"),
+    body("email").isEmail().normalizeEmail().trim().escape().withMessage("Email is required"),
+    body("password").isLength({ min: 5 }).trim().withMessage("password should be 6 characters long")
+];
