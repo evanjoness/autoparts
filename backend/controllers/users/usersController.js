c2aab5317e0ad0c6772254ac544a882462fad5d0
ivan shpytchuk <ivan7shpytchuk@gmail.com>
2023-04-01 12:24:26 +0300

    Login API

--- backend/controllers/users/usersController.js
+++ backend/controllers/users/usersController.js
@@ -1,6 +1,6 @@
 const { validationResult } = require("express-validator");
 const UserModel = require("../../models/User");
-const { hashedPassword, createToken } = require("../../services/authServices")
+const { hashedPassword, createToken , comparePassword} = require("../../services/authServices")
 //@route POST /api/register
 //@access Public 
 //@desc Create user and return a token 
@@ -34,4 +34,38 @@ module.exports.register = async (req, res) => {
         //validation failed 
         return res.status(400).json({ errors: errors.array() })
     }
+}
+
+//@route POST /api/login
+//@access Public 
+//@desc Login user and return a token 
+
+module.exports.login = async (req, res) =>{
+     const {email, password} = req.body;
+     const errors = validationResult(req);
+     if(errors.isEmpty()){
+        try {
+            const user = await UserModel.findOne({email});
+            if(user){
+                if(await comparePassword(password, user.password)){
+                    const token = createToken({id:user._id, name:user.name});
+                    if(user.admin){
+                        return res.status(201).json({token, admin:true})
+                    }else{
+                        return res.status(201).json({token, admin:false})
+                    }
+                }else{
+                    return res.status(400).json({errors:[{msg:"password not matched", param:"password"}]})
+                }
+            }else{
+                return res.status(400).json({errors:[{msg:`${email} is not found`, param:"email"}]})
+            }
+        } catch (error) {
+            console.log(error.message)
+            return res.status(500).json("server internal error")
+        }
+     }else{
+        //validation failed
+        return res.status(401).json({errors:errors.array()});
+     }
 }
