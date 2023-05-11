5c72a566ffaf162c609cb05cf58019e4c7345738
ivan shpytchuk <ivan7shpytchuk@gmail.com>
2023-05-11 17:43:14 +0300

    fetch categories api

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const categoryValidations = require("../validations/categoryValidations");
-const Category = require("../controllers/Category")
-const Authorization = require("../services/Authorization")
-router.post("/create-category", [categoryValidations, Authorization.authorized], Category.create)
+const Category = require("../controllers/Category");
+const Authorization = require("../services/Authorization");
+router.post("/create-category", [categoryValidations, Authorization.authorized], Category.create);
+router.get("/categories/:page",  Category.categories);
 module.exports = router;
