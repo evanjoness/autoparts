3a093b4f7d108344bfe1f7cf952c2e533902f0f8
ivan shpytchuk <ivan7shpytchuk@gmail.com>
2023-06-11 09:06:05 +0300

    all brands in slides

---
+++ client/src/components/skeleton/Skeleton.js
@@ -0,0 +1,8 @@
+
+const Skeleton = ({children}) => {
+  return (
+    <div className="w-full overflow-hidden">{children}</div>
+  )
+}
+
+export default Skeleton
--- client/src/screens/home/Home.js
+++ client/src/screens/home/Home.js
@@ -2,12 +2,16 @@
 
 import Slider from "../../components/home/Slider";
 import Nav from "../../components/home/Nav";
+import Brands from "../../components/home/Brands";
 const Home = () => {
   return (
     <>
         <Nav/>
         <div className="mt-[70px]">
           <Slider/>
+        </div>
+        <div className="my-container mt-10">
+          <Brands/>
         </div>
     </>
 
